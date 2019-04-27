0b5fedb4fb00ebafbd8b6346f8e8c024ce631e9e
zhaojintian <729234283@qq.com>
2019-04-27 23:09:55 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 module.exports = {
   Auth: require('./modules/auth'),
-  Payment: require('./modules/payment')
+  Payment: require('./modules/payment'),
+  corporation: {
+    Auth: require('./modules/corporation/auth')
+  }
 }
---
+++ src/modules/corporation/auth/index.js
@@ -0,0 +1,43 @@
+const axios = require('axios')
+const WXBizDataCrypt = require('./utils/WXBizDataCrypt')
+
+module.exports = class Auth {
+  constructor ({ appid, secret } = {}) {
+    this.appid = appid
+    this.secret = secret
+  }
+
+  async getAccessToken () {
+    const { data } = await axios.request({
+      method: 'GET',
+      url: 'https://api.weixin.qq.com/cgi-bin/token',
+      params: {
+        appid: this.appid,
+        secret: this.secret,
+        grant_type: 'client_credential'
+      }
+    })
+
+    return data
+  }
+
+  async codeToSession ({ code }) {
+    const { data } = await axios.request({
+      method: 'GET',
+      url: 'https://api.weixin.qq.com/sns/jscode2session',
+      params: {
+        appid: this.appid,
+        secret: this.secret,
+        js_code: code,
+        grant_type: 'authorization_code'
+      }
+    })
+
+    return data
+  }
+
+  decryptData ({ sessionKey, encryptedData, iv }) {
+    return new WXBizDataCrypt(this.appid, sessionKey)
+      .decryptData(encryptedData, iv)
+  }
+}
